b43439d77873e052e30b98dcc7bdf103500bfe70
polymorpher <aaron@qokka.ai>
2022-11-20 23:22:29 -0800

    add apis

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,16 @@
 import Contract from 'web3-eth-contract'
 import config from '../../config'
+import D1DC from '../../abi/D1DC.json'
+import Constants from '../constants'
+import BN from 'bn.js'
+
 const apis = ({ web3, address }) => {
   if (!web3) {
     return
   }
   Contract.setProvider(web3.currentProvider)
+  const contract = new Contract(D1DC, config.contract)
+
   return {
     address,
     web3,
@@ -13,6 +19,36 @@ const apis = ({ web3, address }) => {
     },
     purchase: ({ amount }) => {
       return amount
+    },
+    getParameters: async () => {
+      const [baseRentalPrice, rentalPeriod, priceMultiplier] = await Promise.all([
+        contract.methods.baseRentalPrice().call(),
+        contract.methods.rentalPeriod().call(),
+        contract.methods.priceMultiplier().call(),
+      ])
+      return {
+        baseRentalPrice: {
+          amount: new BN(baseRentalPrice).toString(),
+          formatted: web3.utils.fromWei(baseRentalPrice)
+        },
+        rentalPeriod: new BN(rentalPeriod).toNumber() * 1000,
+        priceMultiplier: new BN(priceMultiplier).toNumber()
+      }
+    },
+    getPrice: async ({ name }) => {
+      const nameBytes = web3.utils.keccak256(name)
+      const price = await contract.methods.getPrice(nameBytes).call()
+      const amount = new BN(price).toString()
+      return {
+        amount,
+        formatted: web3.utils.fromWei(amount)
+      }
+    },
+    getRecord: async ({ name }) => {
+      const nameBytes = web3.utils.keccak256(name)
+      const result = await contract.methods.nameRecords(nameBytes).call()
+      const [renter, lastPrice, timeUpdated, url] = Object.keys(result).map(k => result[k])
+      return { renter: renter === Constants.EmptyAddress ? null : renter, lastPrice, timeUpdated, url }
     }
   }
 }
