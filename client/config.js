306b8577c311f74bfc88c9fd04dd0d45987f6ce8
polymorpher <aaron@qokka.ai>
2022-11-20 23:22:16 -0800

    cleanup config

--- client/config.js
+++ client/config.js
@@ -2,16 +2,10 @@ const debug = process.env.DEBUG
 
 const config = {
   debug,
-  burnerContract: process.env.BURNER_CONTRACT || (debug ? '0x9BC52FBcCcde8cEADAEde51a25dBeD489b201e53' : '0x476e14D956dca898C33262aecC81407242f8431A'),
-  previousBurnerContracts: JSON.parse(process.env.PREVIOUS_BURNER_CONTRACTS || '[]'),
+  contract: process.env.CONTRACT || (debug ? '0x9BC52FBcCcde8cEADAEde51a25dBeD489b201e53' : '0x476e14D956dca898C33262aecC81407242f8431A'),
   explorer: process.env.EXPLORER_URL || 'https://explorer.harmony.one/#/tx/{{txId}}',
   defaultRPC: process.env.DEFAULT_RPC || 'https://api.harmony.one',
-  disabledAssets: JSON.parse(process.env.DISABLED_ASSETS || '[]'),
-  supportedAssets: process.env.SUPPORTED_ASSETS
-    ? JSON.parse(process.env.SUPPORTED_ASSETS)
-    : [
-        debug ? '0xe3BCFeeE8a783F1c107C71385652dC2Ac0662598' : '0x985458E523dB3d53125813eD68c274899e9DfAb4',
-      ],
+  tld: process.env.TLD || '.1.country',
   chainParameters: process.env.CHAIN_PARAMETERS
     ? JSON.parse(process.env.CHAIN_PARAMETERS)
     : {
