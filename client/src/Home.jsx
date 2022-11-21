13ca8172fe0617e569f79b41794fca73ec4f536b
polymorpher <aaron@qokka.ai>
2022-11-21 01:06:04 -0800

    fix frontend wip

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,8 +3,8 @@ import Web3 from 'web3'
 import detectEthereumProvider from '@metamask/detect-provider'
 import config from '../config'
 import { Button, LinkWrarpper } from './components/Controls'
-import { BaseText, Title } from './components/Text'
-import { FlexColumn, FlexRow, Main } from './components/Layout'
+import { BaseText, SmallText, Title } from './components/Text'
+import { Col, FlexColumn, FlexRow, Main, Row } from './components/Layout'
 import { TwitterTweetEmbed } from 'react-twitter-embed'
 import styled from 'styled-components'
 
@@ -13,16 +13,52 @@ import apis from './api'
 
 const Container = styled(Main)`
   margin: 32px auto;
+  padding: 16px;
   max-width: 800px;
-
   // TODO: responsive
 `
 
-const Home = ({ name = 'test', subdomain = 'test', tweetId = '933354946111705097', price = 100, lastPurchaseDate = new Date() }) => {
+const Label = styled(SmallText)`
+  margin-right: 16px;
+  color: grey;
+`
+
+const getSubdomain = () => {
+  if (!window) {
+    return null
+  }
+  const host = window.location.host
+  const parts = host.split('.')
+  if (parts.length < 2) {
+    return null
+  }
+  if (parts.length <= 3) {
+    return parts[0]
+  }
+  return parts.slice(0, parts.length - 2).join('.')
+}
+
+const Home = ({ subdomain = config.tld }) => {
   const [web3, setWeb3] = useState()
   const [provider, setProvider] = useState()
   const [address, setAddress] = useState()
   const [client, setClient] = useState(apis({}))
+  const [record, setRecord] = useState(null)
+  const [price, setPrice] = useState(null)
+  const [parameters, setParameters] = useState()
+  const [tweetId, setTweetId] = useState('933354946111705097')
+
+  const name = getSubdomain()
+
+  const switchChain = async () => {
+    return window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: config.chainParameters.chainId }],
+    }).then(() => {
+      toast.success(`Switched to network: ${config.chainParameters.chainName}`)
+      setClient(apis({ web3, address }))
+    })
+  }
 
   async function init () {
     const provider = await detectEthereumProvider()
@@ -45,12 +81,7 @@ const Home = ({ name = 'test', subdomain = 'test', tweetId = '933354946111705097
       setAddress(address)
 
       try {
-        await window.ethereum.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: config.chainParameters.chainId }],
-        })
-        toast.success(`Switched to network: ${config.chainParameters.chainName}`)
-        setClient(apis({ web3, address }))
+        await switchChain()
       } catch (ex) {
         console.error(ex)
         if (ex.code !== 4902) {
@@ -91,8 +122,9 @@ const Home = ({ name = 'test', subdomain = 'test', tweetId = '933354946111705097
     if (!client) {
       return
     }
-    // any other initialization on the client
-    console.log(client)
+    client.getParameters().then(p => setParameters(p))
+    client.getRecord({ name }).then(r => setRecord(r))
+    client.getPrice({ name }).then(p => setPrice(p))
   }, [client])
 
   const onBuy = async () => {
@@ -124,26 +156,37 @@ const Home = ({ name = 'test', subdomain = 'test', tweetId = '933354946111705097
           {subdomain}
         </BaseText>
       </FlexRow>
-      {lastPurchaseDate && (
-        <FlexColumn>
-          <BaseText>Price: {price}</BaseText>
-          <BaseText>Purchased on: {lastPurchaseDate.toString()}</BaseText>
-        </FlexColumn>
-      )}
-      {tweetId && (
-        <FlexRow className='tweetContainer'>
-          <TwitterTweetEmbed tweetId={tweetId} />
-        </FlexRow>)}
+      {record?.renter &&
+        <>
+          <Col>
+            <Row>
+              <Label>Price</Label><BaseText>{price?.formatted} ONE</BaseText>
+            </Row>
+            <Row>
+              <Label>Last purchase</Label><BaseText> {new Date(record.timeUpdated).toLocaleString()}</BaseText>
+            </Row>
+          </Col>
+          <FlexRow className='tweet-container'>
+            <TwitterTweetEmbed tweetId={tweetId} />
+          </FlexRow>
+        </>}
+      {!record?.renter &&
+        <Col>
+          <Title>Page Not Yet Claimed</Title>
+          <BaseText style={{ marginTop: 32, fontSize: 12, color: 'grey', marginLeft: '16px', textAlign: 'center' }}>
+            Claim now and embed a tweet you choose
+          </BaseText>
+          <Row style={{ marginTop: 32, justifyContent: 'center' }}>
+            <Label>Price</Label><BaseText>{price?.formatted} ONE</BaseText>
+          </Row>
+        </Col>}
 
       {!address && <Button onClick={connect} style={{ width: 'auto' }}>CONNECT METAMASK</Button>}
 
       {address && config.debug && <BaseText>Your address: {address}</BaseText>}
-      {/* TODO: allow change bid price */}
+
       {address && (
-        <Button onClick={onBuy}>
-          Buy
-          <BaseText style={{ fontSize: '10px', display: 'inline', marginLeft: '8px' }}>({price * 2})</BaseText>
-        </Button>
+        <Button onClick={onBuy}>Buy</Button>
       )}
     </Container>
   )
