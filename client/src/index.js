8ede865ee3c08aeb82f38580fb19b0f1cdd5ce2a
fegloff <fegloff@gmail.com>
2022-12-14 21:22:44 -0500

    add metadata for page preview + favico + image set for android and iphone

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { ToastContainer } from 'react-toastify'
 import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { Helmet } from 'react-helmet'
+
 import config from '../config'
 
 import 'react-toastify/dist/ReactToastify.css'
@@ -28,6 +30,11 @@ export const ethereumClient = new EthereumClient(wagmiClient, chains)
 
 ReactDOM.render(
   <WagmiConfig client={wagmiClient}>
+    <Helmet>
+      <meta charSet='utf-8' />
+      <title>s.country</title>
+      <meta name='description' content='Harmony’s .1.country unifies Internet domains and crypto names as Web3 identities. Short, onchain names like s.1 store your wallet addresses, digitial collectibles, social reputation – on Harmony across multiple blockchains. Proper, browsable domains like s.country displays your career metrics, vanity links, embedded content – for fans to tips with emojis or pay for work. Yet, s.1 is magically the same as s.country – your creator economy with ONE!' />
+    </Helmet>
     <Routes />
     <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     <ToastContainer position='top-left' />
