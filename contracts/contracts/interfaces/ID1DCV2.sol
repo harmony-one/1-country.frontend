a570b24980b32f288664802e18c1666e37cfb0bc
apptreeso <superwebdev19@gmail.com>
2022-12-26 12:01:48 +0900

    update nameRecords function interface

--- contracts/contracts/interfaces/ID1DCV2.sol
+++ contracts/contracts/interfaces/ID1DCV2.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 interface ID1DCV2 {
     function nameRecords(bytes32 name)
         external
+        view
         returns (
             address renter,
             uint32 timeUpdated,
