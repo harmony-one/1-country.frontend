de746d25ed49ef7feb2878f0681c5606379f4383
apptreeso <superwebdev19@gmail.com>
2022-12-26 11:48:14 +0900

    add nameRecords function to ID1DCV2 interface

---
+++ contracts/contracts/interfaces/ID1DCV2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+interface ID1DCV2 {
+    function nameRecords(bytes32 name)
+        external
+        returns (
+            address renter,
+            uint32 timeUpdated,
+            uint256 lastPrice,
+            string memory url,
+            string memory prev,
+            string memory next
+        );
+}
