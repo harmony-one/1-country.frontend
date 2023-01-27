6945a415c10c008f5b553ef699016c9d2746a9c4
ahiipsa <ahiipsa@gmail.com>
2023-01-27 23:13:56 +0400

    Removed BUY button

--- client/src/routes/home/components/WidgetSellProduct.jsx
+++ client/src/routes/home/components/WidgetSellProduct.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
-import styled from 'styled-components'
 import { WidgetContainer } from './WidgetContainer'
 import { WidgetBackground } from './WidgetBackground'
 import { WidgetHead } from './WidgetHead'
 import { WidgetLikes } from './WidgetLikes'
 
-const ButtonBuy = styled.button`
-  border-radius: 10px;
-  background-color: #333333;
-  color: white;
-  padding: 4px 12px;
-  border: none;
-`
-
 export const WidgetDirectSell = () => {
   return (
     <WidgetContainer>
-      <WidgetBackground image='https://static.wixstatic.com/media/b40d8c_5e0b10ec2fed43e397dfa0e4d7dfd19b~mv2.jpg/v1/fill/w_584,h_584,al_c,q_85,usm_0.66_1.00_0.01/b40d8c_5e0b10ec2fed43e397dfa0e4d7dfd19b~mv2.jpg' />
-      <WidgetHead>
-        <ButtonBuy><a style={{color: 'white', textDecoration: 'none' }} href={"https://www.amazon.com/dp/B0BSP2HXQT"}> BUY</a></ButtonBuy>
-        <WidgetLikes />
-      </WidgetHead>
+      <a href='https://www.amazon.com/dp/B0BSP2HXQT'>
+        <WidgetBackground image='https://static.wixstatic.com/media/b40d8c_5e0b10ec2fed43e397dfa0e4d7dfd19b~mv2.jpg/v1/fill/w_584,h_584,al_c,q_85,usm_0.66_1.00_0.01/b40d8c_5e0b10ec2fed43e397dfa0e4d7dfd19b~mv2.jpg' />
+
+        <WidgetHead>
+          <WidgetLikes />
+        </WidgetHead>
+      </a>
     </WidgetContainer>
   )
 }
