5544ec9187796b34fa6480a87c94a678fed38397
ahiipsa <ahiipsa@gmail.com>
2023-02-03 14:46:02 +0400

    Added prettier

--- client/src/components/modals/ModalWidgetAdd.tsx
+++ client/src/components/modals/ModalWidgetAdd.tsx
@@ -1,40 +1,50 @@
-import React from "react";
-import {observer} from "mobx-react-lite";
-import {ModalContent} from "./ModalContent";
-import styled from "styled-components";
-import {Box, BoxProps} from "grommet";
-import {BaseText, Title} from "../Text";
+import React from 'react'
+import { observer } from 'mobx-react-lite'
+import { ModalContent } from './ModalContent'
+import styled from 'styled-components'
+import { Box, BoxProps } from 'grommet'
+import { BaseText, Title } from '../Text'
 import {
   AiOutlinePicture,
   AiOutlineShoppingCart,
   AiOutlineTwitter,
   AiOutlineVideoCamera,
-  IoTextSharp
-} from "react-icons/all";
-import {ModalHeader} from "./ModalHeader";
-import {useStores} from "../../stores";
-import {ModalIds} from "../../modules/modals";
-
+  IoTextSharp,
+} from 'react-icons/all'
+import { ModalHeader } from './ModalHeader'
+import { useStores } from '../../stores'
+import { ModalIds } from '../../modules/modals'
 
 interface CardProps {
-  onClick?: () => void;
-  disabled?: boolean;
+  onClick?: () => void
+  disabled?: boolean
   children: React.ReactNode | React.ReactNode[]
 }
 
-const StyledBox = styled(Box)<BoxProps & {disabled?: boolean}>`
+const StyledBox = styled(Box)<BoxProps & { disabled?: boolean }>`
   cursor: pointer;
-  background-color: ${props => props.disabled ? '#ECEDF5' : 'white' };
-  
+  background-color: ${(props) => (props.disabled ? '#ECEDF5' : 'white')};
+
   &:hover {
     background-color: beige;
   }
 `
 
-const Card: React.FC<CardProps> = ({children, disabled, onClick}) => {
-  return <StyledBox disabled={disabled} onClick={onClick} pad="16px" round="8px" elevation="medium" align="center" justify="center" gap="8px">
-    {children}
-  </StyledBox>
+const Card: React.FC<CardProps> = ({ children, disabled, onClick }) => {
+  return (
+    <StyledBox
+      disabled={disabled}
+      onClick={onClick}
+      pad="16px"
+      round="8px"
+      elevation="medium"
+      align="center"
+      justify="center"
+      gap="8px"
+    >
+      {children}
+    </StyledBox>
+  )
 }
 
 const Grid = styled.div`
@@ -45,15 +55,14 @@ const Grid = styled.div`
 `
 
 interface Props {
-  onClose?: () => void;
+  onClose?: () => void
 }
 
-export const ModalWidgetAdd: React.FC<Props> = observer(({onClose}) => {
-
-  const {modalStore} = useStores();
+export const ModalWidgetAdd: React.FC<Props> = observer(({ onClose }) => {
+  const { modalStore } = useStores()
 
   const handleClick = () => {
-    modalStore.showModal(ModalIds.PROFILE_ADD_WIDGET_TEXT);
+    modalStore.showModal(ModalIds.PROFILE_ADD_WIDGET_TEXT)
   }
 
   return (
@@ -63,7 +72,7 @@ export const ModalWidgetAdd: React.FC<Props> = observer(({onClose}) => {
         <Title>Add widget</Title>
         <Grid>
           <Card onClick={handleClick}>
-            <IoTextSharp  size="40px"/>
+            <IoTextSharp size="40px" />
             <BaseText>Text</BaseText>
           </Card>
           <Card disabled>
@@ -75,11 +84,11 @@ export const ModalWidgetAdd: React.FC<Props> = observer(({onClose}) => {
             <BaseText>Video</BaseText>
           </Card>
           <Card disabled>
-            <AiOutlineShoppingCart  size="40px" />
+            <AiOutlineShoppingCart size="40px" />
             <BaseText>Merchandise</BaseText>
           </Card>
           <Card disabled>
-            <AiOutlinePicture  size="40px" />
+            <AiOutlinePicture size="40px" />
             <BaseText>NFT</BaseText>
           </Card>
         </Grid>
