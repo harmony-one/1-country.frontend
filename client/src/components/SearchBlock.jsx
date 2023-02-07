7d20b2d6c32c00347369534a57c9d91836c81483
ahiipsa <ahiipsa@gmail.com>
2023-02-07 20:17:13 +0400

    Added validation for a domain name

--- client/src/components/SearchBlock.jsx
+++ client/src/components/SearchBlock.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import debounce from 'lodash.debounce'
 import { Button } from './Controls'
 import { SearchResultItem } from './SearchResultItem'
+import { BaseText } from './Text'
 
 const Container = styled.div`
   width: 100%;
@@ -11,7 +12,7 @@ const Container = styled.div`
 const InputContainer = styled.div`
   position: relative;
   box-sizing: border-box;
-  border: 2px solid #CFCFCF;
+  border: 2px solid ${(props) => (props.valid ? '#cfcfcf' : '#ff8c8c')};
   display: flex;
   align-items: center;
   overflow: hidden;
@@ -25,17 +26,25 @@ const StyledInput = styled.input`
   margin: 0 8px 0 12px;
   padding: 0;
   width: 100%;
-  
+
   &:focus {
     outline: none;
   }
 `
+
+const regx =
+  /^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/
+
+const isValidDomainName = (domainName) => {
+  return regx.test(domainName + '.1.country')
+}
 
 export const SearchBlock = ({ client }) => {
   const [search, setSearch] = useState('')
   const [loading, setLoading] = useState()
   const [price, setPrice] = useState()
   const [record, setRecord] = useState()
+  const [isValid, setIsValid] = useState(true)
   const [recordName, setRecordName] = useState('')
   const [parameters, setParameters] = useState({
     rentalPeriod: 0,
@@ -51,7 +60,12 @@ export const SearchBlock = ({ client }) => {
 
   const handleSearchChange = (event) => {
     setSearch(event.target.value)
-    handleSearch(event.target.value)
+    const _isValid = isValidDomainName(event.target.value)
+    setIsValid(_isValid)
+
+    if (_isValid) {
+      handleSearch(event.target.value)
+    }
   }
 
   const handleSearch = useMemo(() => {
@@ -62,12 +76,15 @@ export const SearchBlock = ({ client }) => {
 
       setLoading(true)
 
-      client.getRecord({ name: search }).then((r) => {
-        setRecord(r)
-        setLoading(false)
-      }).catch(ex => {
-        console.log('### ex', ex)
-      })
+      client
+        .getRecord({ name: search })
+        .then((r) => {
+          setRecord(r)
+          setLoading(false)
+        })
+        .catch((ex) => {
+          console.log('### ex', ex)
+        })
       client.getPrice({ name: search }).then((p) => {
         setPrice(p)
       })
@@ -76,13 +93,32 @@ export const SearchBlock = ({ client }) => {
     }, 500)
   }, [client])
 
+  const handlePay = () => {
+    if (!record || !isValid) {
+      return false
+    }
+  }
+
   return (
     <Container>
-      <InputContainer>
+      <InputContainer valid={isValid}>
         {/* <AiOutlineSearch size='24px' style={{ margin: '0px 8px 0px 12px' }} /> */}
-        <StyledInput placeholder='harmony.1' value={search} onChange={handleSearchChange} />
-        <Button style={{ marginLeft: 'auto' }} onClick={handleSearchChange}>Pay</Button>
+        <StyledInput
+          placeholder="harmony.1"
+          value={search}
+          valid={isValid}
+          onChange={handleSearchChange}
+        />
+        <Button
+          disabled={!isValid}
+          style={{ marginLeft: 'auto' }}
+          onClick={handlePay}
+        >
+          Pay
+        </Button>
       </InputContainer>
+
+      {!isValid && <BaseText>Invalid domain name</BaseText>}
       {/* <div>1 ONE = ($1.20 USD) for 3 months</div> */}
 
       {/* <SearchResultItem */}
@@ -99,17 +135,16 @@ export const SearchBlock = ({ client }) => {
       {/*  period={86400000} */}
       {/* /> */}
 
-      {loading && (
-        <div>Loading...</div>
-      )}
+      {loading && <div>Loading...</div>}
 
-      {!loading && record && price &&
+      {isValid && !loading && record && price && (
         <SearchResultItem
           name={recordName}
           price={price.formatted}
           available={!record.renter}
           period={parameters.rentalPeriod}
-        />}
+        />
+      )}
     </Container>
   )
 }
