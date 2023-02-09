3838db8ac00e000134c2966f9144974cce9854be
ahiipsa <ahiipsa@gmail.com>
2023-02-09 21:22:57 +0400

    Migrate WalletStore.ts to wagmi/core

--- client/src/stores/WalletStore.ts
+++ client/src/stores/WalletStore.ts
@@ -1,27 +1,48 @@
 import { BaseStore } from './BaseStore'
 import { RootStore } from './RootStore'
-import { makeObservable, observable } from 'mobx'
-import { wagmiClient } from '../modules/wagmi/wagmiClient'
+import { computed, makeObservable, observable } from 'mobx'
+import { connect, GetAccountResult, watchAccount } from '@wagmi/core'
+import { metamaskConnector } from '../modules/wagmi/wagmiClient'
 
 export class WalletStore extends BaseStore {
-  isConnected: boolean = false
-  walletAddress: string = ''
+  _account: GetAccountResult = {
+    address: undefined,
+    connector: undefined,
+    isConnected: false,
+    isReconnecting: false,
+    isConnecting: false,
+    isDisconnected: true,
+    status: 'disconnected',
+  }
 
   constructor(rootStore: RootStore) {
     super(rootStore)
     makeObservable(
       this,
       {
-        isConnected: observable,
-        walletAddress: observable,
+        _account: observable,
+        isConnected: computed,
+        walletAddress: computed,
       },
       { autoBind: true }
     )
+
+    watchAccount((account) => {
+      this._account = account
+    })
+  }
+
+  get isConnected() {
+    return this._account.isConnected
+  }
+
+  get walletAddress() {
+    return this._account.address
   }
 
   connect() {
-    // TODO: fix wagmi connectors
-    // @ts-expect-error
-    return wagmiClient.connectors.connect()
+    return connect({
+      connector: metamaskConnector,
+    })
   }
 }
