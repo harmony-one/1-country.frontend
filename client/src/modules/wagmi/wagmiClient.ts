ee030332da360ab4877ef1847bdb188db25b0d49
ahiipsa <ahiipsa@gmail.com>
2023-02-09 21:22:24 +0400

    Migrated wagmi to typescript

--- client/src/modules/wagmi/wagmiClient.ts
+++ client/src/modules/wagmi/wagmiClient.ts
@@ -1,17 +1,14 @@
 import { configureChains, createClient } from 'wagmi'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { publicProvider } from 'wagmi/providers/public'
-// import { InjectedConnector } from 'wagmi/connectors/injected'
-// import { modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
 import config from '../../../config'
 
 const chains = [config.chainParameters]
 
 const { provider } = configureChains(chains, [publicProvider()]) //, [walletConnectProvider({ projectId: config.walletConnect.projectId })])
-console.log('PROVIDR ODSFSFF', chains, provider)
+
+export const metamaskConnector = new MetaMaskConnector({ chains })
 export const wagmiClient = createClient({
-  // autoConnect: true,
-  connectors: new MetaMaskConnector({ chains }),
-  // modalConnectors({ appName: 'web3Modal', chains }),
+  connectors: [metamaskConnector],
   provider,
 })
