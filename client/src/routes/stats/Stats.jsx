2f47286c853082d6ebde3160d7b1ad0d77ac23b9
abhi5456 <abhimamidipaka@gmail.com>
2023-02-15 02:20:39 -0800

    minor stat fix

--- client/src/routes/stats/Stats.jsx
+++ client/src/routes/stats/Stats.jsx
@@ -8,17 +8,17 @@ const Stats = observer(() => {
   return (
     <Container>
       {(
-        <FlexColumn style={{ width: '100%', alignItems: 'center' }}>
+        <FlexColumn style={{ width: '100%', height: '100%', alignItems: 'center' }}>
           <h2>{`Domain Stats`}</h2>
           <div id="main_block">
-          <iframe
-            src="https://www.footprint.network/public/dashboard/Domain-Sales-fp-989a5037-a18e-4851-b68c-e38fcb006a74"
-            frameborder="0"
-            width="1000"
-            height="650"
-            allowtransparency>
-          </iframe>
-        
+            <iframe
+              src="https://www.footprint.network/public/dashboard/Domain-Sales-fp-989a5037-a18e-4851-b68c-e38fcb006a74"
+              frameborder="0"
+              width="1000"
+              height="650"
+              allowtransparency>
+            </iframe>
+          
         
           <br></br>
           <br></br>
