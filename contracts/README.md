66ce80ad0fa4e170553690eda89846292db02183
ahiipsa <ahiipsa@gmail.com>
2023-02-17 18:35:40 +0400

    Clear project Resolve #7

--- contracts/README.md
+++
@@ -1,5 +0,0 @@
-# ONE Country Manager Contracts
-
-
-## Overview
-This repository holds the contracts to manage the renting of .1.country domains.
--- contracts/scripts/sdoc.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-slither . --print inheritance-graph
-mv *.dot ./docs/slither/.
-cd ./docs/slither
-dot inheritance-graph.dot -Tpng -o inheritance-graph.png
-rm *.dot
-cd ../..
