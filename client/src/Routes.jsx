2f55a01fadb5f50b5267b640beb7258f2d295955
ahiipsa <ahiipsa@gmail.com>
2023-02-18 02:39:24 +0400

    Removed useless routes

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './routes/home/Home'
-import { OpenWidgetsPage } from './routes/open-widgets/OpenWidgetsPage'
-import WaitingRoom from './routes/waiting-room/WaitingRoom'
 import Stats from './routes/stats/Stats'
 import { PageNotFound } from './routes/404/404Page'
 
@@ -11,8 +9,6 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="stats/" element={<Stats />} />
-      <Route path="new/:domainName" element={<WaitingRoom />} />
-      <Route path="home/" element={<OpenWidgetsPage />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
