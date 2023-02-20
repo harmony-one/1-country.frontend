0e0f75703415323626b73348c9605130e8b12863
ahiipsa <ahiipsa@gmail.com>
2023-02-20 19:03:00 +0400

    Removed polyfills

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,8 +54,8 @@ module.exports = {
               [
                 '@babel/preset-env',
                 {
-                  useBuiltIns: 'usage',
-                  corejs: 3,
+                  useBuiltIns: false,
+                  // corejs: 3,
                   modules: 'cjs',
                 },
               ],
