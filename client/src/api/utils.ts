a14a2e50f20aad99f128f1c7c50b4d4424148e3e
ahiipsa <ahiipsa@gmail.com>
2023-02-27 11:35:24 +0400

    Fixed dc utils

--- client/src/api/utils.ts
+++ client/src/api/utils.ts
@@ -31,7 +31,7 @@ export const nameUtils = {
 }
 
 export const utils = {
-  hexView: (bytes) => {
+  hexView: (bytes: Uint8Array) => {
     return (
       bytes &&
       Array.from(bytes)
@@ -40,65 +40,25 @@ export const utils = {
     )
   },
 
-  hexString: (bytes) => {
+  hexString: (bytes: Uint8Array) => {
     return '0x' + utils.hexView(bytes)
   },
 
-  keccak: (bytes) => {
+  keccak: (bytes: Uint8Array) => {
     const k = createKeccakHash('keccak256')
     // assume Buffer is poly-filled or loaded from https://github.com/feross/buffer
     const hash = k.update(Buffer.from(bytes)).digest()
     return new Uint8Array(hash)
   },
 
-  stringToBytes: (str) => {
+  stringToBytes: (str: string) => {
     return new TextEncoder().encode(str)
   },
 
-  keccak256: (str, use0x) => {
+  keccak256: (str: string, use0x: boolean) => {
     const bytes = utils.stringToBytes(str)
     const hash = utils.keccak(bytes)
     return use0x ? utils.hexString(hash) : utils.hexView(hash)
-  },
-
-  hexToBytes: (hex, length, padRight) => {
-    if (!hex) {
-      return
-    }
-    length = length || hex.length / 2
-    const ar = new Uint8Array(length)
-    for (let i = 0; i < hex.length / 2; i += 1) {
-      let j = i
-      if (padRight) {
-        j = length - hex.length + i
-      }
-      ar[j] = parseInt(hex.slice(i * 2, i * 2 + 2), 16)
-    }
-    return ar
-  },
-
-  hexStringToBytes: (hexStr, length) => {
-    return hexStr.startsWith('0x')
-      ? utils.hexToBytes(hexStr.slice(2), length)
-      : utils.hexToBytes(hexStr, length)
-  },
-
-  tryNormalizeAddress: (address) => {
-    try {
-      return web3.utils.toChecksumAddress((address || '').toLowerCase())
-    } catch (ex) {
-      console.error(ex)
-      return null
-    }
-  },
-
-  ecrecover: (message, signature) => {
-    try {
-      return web3.eth.accounts.recover(message, signature)
-    } catch (ex) {
-      console.error(ex)
-      return null
-    }
   },
 }
 
