b2d8f0c56600daaeb35f1cee483652d20d776bf7
abhi5456 <abhimamidipaka@gmail.com>
2023-03-02 14:48:34 -0800

    change startdelay

--- client/src/routes/home/components/Typed.tsx
+++ client/src/routes/home/components/Typed.tsx
@@ -8,11 +8,11 @@ export const TypedText: React.FC<Props> = React.memo(() => {
 
   useEffect(() => {
     new Typed(ref.current, {
-      strings: ['harmony', 'nft', 'crypto', 'web3'],
+      strings: ['harmony', 'nft', 'crypto', 'web3', 'defi'],
       typeSpeed: 100,
       backSpeed: 90,
       backDelay: 500,
-      startDelay: 1000,
+      startDelay: 0,
       // fadeOut: true,
       loop: true,
     })
