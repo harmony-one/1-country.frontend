fdf8ef0f747ddd03b3c4805b77d3301ee166d996
ahiipsa <ahiipsa@gmail.com>
2023-03-02 18:09:58 +0400

    Fixed wallet connect after page refresh

--- client/src/modules/wagmi/wagmiClient.ts
+++ client/src/modules/wagmi/wagmiClient.ts
@@ -7,17 +7,19 @@ import config from '../../../config'
 const chains = [config.chainParameters]
 const { provider } = configureChains(chains, [
   publicProvider(),
-  walletConnectProvider({ projectId: config.walletConnect.projectId })
+  walletConnectProvider({ projectId: config.walletConnect.projectId }),
 ])
 
 // walletConnect with modern UI
-const web3ModalConnectors = modalConnectors({ appName: 'web3Modal', chains })
-  .filter((connector) => connector.id === 'walletConnect')
+const web3ModalConnectors = modalConnectors({
+  appName: 'web3Modal',
+  chains,
+}).filter((connector) => connector.id === 'walletConnect')
 
 export const metamaskConnector = new MetaMaskConnector({ chains })
 export const [walletConnectConnector] = web3ModalConnectors
 export const wagmiClient = createClient({
-  autoConnect: false,
+  autoConnect: true,
   connectors: [metamaskConnector, walletConnectConnector],
-  provider
+  provider,
 })
