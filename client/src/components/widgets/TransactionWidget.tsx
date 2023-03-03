ecfe5a5487bca3b7de7416650a6f0d2aa4cbb1a5
artemkolodko <temakolodko@gmail.com>
2023-03-03 21:08:04 +0000

    Add mobile styles

--- client/src/components/widgets/TransactionWidget.tsx
+++ client/src/components/widgets/TransactionWidget.tsx
@@ -8,6 +8,7 @@ import {HarmonyLink} from '../HarmonyLink'
 import { WidgetsContainer } from './Widgets.styles'
 
 const Container = styled(WidgetsContainer)`
+  gap: 0;
   flex-direction: row;
   border: 1px solid rgb(207, 217, 222);
   border-radius: 12px;
@@ -16,12 +17,27 @@ const Container = styled(WidgetsContainer)`
   cursor: ${(props) => (props.onClick ? 'pointer' : undefined)};
   color: rgb(83, 100, 113);
   overflow: hidden;
+  min-width: 278px;
 `
 
 const dateFormat = new Intl.DateTimeFormat('en-US', {
   dateStyle: 'medium',
   timeStyle: 'short',
 })
+
+const ExplorerLogoContainer = styled(Box)`
+  width: 100%;
+  height: 100%;
+  background-color: #00aee9;
+`
+
+const ExplorerLogoWrapper = styled(Box)`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+  }
+`
 
 interface Props {
   isLoading: boolean
@@ -32,25 +48,12 @@ interface Props {
 
 const formatTime = (value: number) => `${value < 10 ? `0${value}` : value}`
 
-const TipContent: React.FC<{ text: string }> = ({ text }) => {
-  return (
-    <Box
-      animation="fadeIn"
-      align="center"
-      border
-      margin={{ horizontal: '8px' }}
-      round="12px"
-      pad={{ horizontal: '16px', vertical: '8px' }}
-      background="white"
-    >
-      <BaseText>{text}</BaseText>
-    </Box>
-  )
-}
-
 const LinkItem = styled.a`
   color: #47b8eb;
   font-size: 14px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `
 
 export const TransactionWidget: React.FC<Props> = ({
@@ -62,30 +65,35 @@ export const TransactionWidget: React.FC<Props> = ({
   const { renter, expirationTime, rentTime } = domainRecord
 
   return <Container style={{ padding: 0 }}>
-    <Box justify={'center'}>
-      <Box align={'start'} pad={'12px'} justify={'center'} background={'#00aee9'} style={{ width: '100%', height: '100%' }}>
+    <ExplorerLogoWrapper justify={'center'}>
+      <ExplorerLogoContainer align={'start'} pad={'12px'} justify={'center'}>
         <Text weight={700} color={'white'} size={'20px'}>
           Harmony
         </Text>
         <Text weight={700} color={'white'} size={'14px'} style={{ whiteSpace: 'nowrap' }}>
           Block Explorer
         </Text>
-      </Box>
-    </Box>
-    <Box gap={'4px'} pad={{ left: '4px', top: '12px', bottom: '12px' }}>
+      </ExplorerLogoContainer>
+    </ExplorerLogoWrapper>
+    <Box gap={'6px'} pad={{ left: '14px', top: '12px', bottom: '12px' }}>
       <Box direction={'row'} gap={'4px'} justify={"start"} align={'center'}>
         <Text size={'small'} weight={'bold'}>Owner:</Text>
         <LinkItem href={`https://explorer.harmony.one/address/${renter}`} target={'_blank'}>{renter}</LinkItem>
       </Box>
       <Box direction={'row'} gap={'4px'} justify={"start"} align={'center'}>
-        <Text size={'small'} weight={'bold'}>Expires in:</Text>
-        <Text size={'small'}>
+        <Text size={'small'} weight={'bold'} style={{ whiteSpace: 'nowrap' }}>Expires in:</Text>
+        <Text size={'small'} style={{ whiteSpace: 'nowrap' }}>
           <Timer
             formatValue={formatTime}
             initialTime={domainRecord.expirationTime - Date.now()}
             direction="backward"
           >
-            <Timer.Days /> days, <Timer.Hours /> hours, <Timer.Minutes /> minutes
+            <Timer.Days /> days
+            {domainRecord.expirationTime - Date.now() < 1000 * 3600 * 24 * 7 ?
+              <span>
+              , <Timer.Hours /> hours, <Timer.Minutes /> min
+              </span>
+            : null}
           </Timer>
         </Text>
       </Box>
