39d00e29bdfda11a970bfce901fb08311114cd95
ahiipsa <ahiipsa@gmail.com>
2023-03-03 18:34:49 +0400

    Fixed page loader

--- client/src/routes/home/components/HomePageLoader.tsx
+++ client/src/routes/home/components/HomePageLoader.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import { FlexColumn } from '../../../components/Layout'
 import { Container } from '../Home.styles'
+import { GradientText } from '../../../components/Text'
 
 interface Props {}
 
 export const HomePageLoader: React.FC<Props> = () => {
   return (
-    <Container>
-      <FlexColumn
-        style={{
-          marginTop: '10em',
-          justifyContent: 'center',
-          alignContent: 'center',
-        }}
-      >
-        Uploading...
-      </FlexColumn>
-    </Container>
+    <div className="preload">
+      <div className="preload-text">.country</div>
+      <div className="preload-loader" />
+    </div>
   )
 }
-
-HomePageLoader.displayName = 'Loader'
