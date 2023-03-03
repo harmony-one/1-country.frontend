4b173d0cff1cd57bf607183800319d90c9f8a3da
ahiipsa <ahiipsa@gmail.com>
2023-03-03 18:14:09 +0400

    Removed cursor from TypedText

--- client/src/routes/home/components/Typed.tsx
+++ client/src/routes/home/components/Typed.tsx
@@ -8,11 +8,28 @@ export const TypedText: React.FC<Props> = React.memo(() => {
 
   useEffect(() => {
     new Typed(ref.current, {
-      strings: ['apes', 'nft', 'food', 'crypto', 'travel', 'web3', 'mine', 'defi', 'fomo', 'wallet', 'bitcoin', 'dao', 'pow', 'hodl', 'token'],
+      strings: [
+        'apes',
+        'nft',
+        'food',
+        'crypto',
+        'travel',
+        'web3',
+        'mine',
+        'defi',
+        'fomo',
+        'wallet',
+        'bitcoin',
+        'dao',
+        'pow',
+        'hodl',
+        'token',
+      ],
       typeSpeed: 100,
       backSpeed: 90,
       backDelay: 500,
       startDelay: 0,
+      cursorChar: '',
       // fadeOut: true,
       loop: true,
     })
