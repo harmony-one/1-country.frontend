799915b07c6235a3e97a9610ce56a25fdd2d85dd
ahiipsa <ahiipsa@gmail.com>
2023-03-10 09:25:53 +0400

    Fixed gromment imports

--- client/src/components/search-input/SearchInput.tsx
+++ client/src/components/search-input/SearchInput.tsx
@@ -1,8 +1,10 @@
-import React, {useRef} from 'react'
-import {Box, TextInput, TextInputProps} from "grommet";
-import { FormClose } from 'grommet-icons/icons/FormClose';
-import styled, {css} from "styled-components";
-import {palette} from "../../constants";
+import React, { useRef } from 'react'
+import { Box } from 'grommet/components/Box'
+import { TextInput, TextInputProps } from 'grommet/components/TextInput'
+
+import { FormClose } from 'grommet-icons/icons/FormClose'
+import styled, { css } from 'styled-components'
+import { palette } from '../../constants'
 
 const TextInputWrapper = styled(TextInput)<{ isValid?: boolean }>`
   border-radius: 20px;
@@ -12,22 +14,24 @@ const TextInputWrapper = styled(TextInput)<{ isValid?: boolean }>`
   color: #333437;
   transition: border-color 250ms, box-shadow 250ms;
 
-  &:focus, &:hover {
+  &:focus,
+  &:hover {
     background-color: #fff;
     box-shadow: 0 1px 5px rgb(32 33 36 / 26%);
-    border-color: rgba(223,225,229,0);
+    border-color: rgba(223, 225, 229, 0);
   }
 
-  ${(props) => (!props.isValid &&
+  ${(props) =>
+    !props.isValid &&
     css`
       border-color: ${palette.PinkRed};
 
-      &:hover, &:focus {
+      &:hover,
+      &:focus {
         border-color: ${palette.LightRed};
-        box-shadow:0 1px 6px rgb(255 77 79 / 26%);
+        box-shadow: 0 1px 6px rgb(255 77 79 / 26%);
       }
-    `
-  )}
+    `}
 `
 
 const InputSuffix = styled(Box)`
@@ -45,17 +49,18 @@ const Container = styled(Box)<{ paddingLeft?: string | null }>`
   position: relative;
 
   input {
-    ${(props) => (props.paddingLeft &&
-        css`
-          padding-left: ${props.paddingLeft};
-        `)}
+    ${(props) =>
+      props.paddingLeft &&
+      css`
+        padding-left: ${props.paddingLeft};
+      `}
   }
 `
 
 export interface SearchInputProps extends TextInputProps {
-  isValid?: boolean;
-  allowClear?: boolean;
-  onSearch?: (value: string) => void;
+  isValid?: boolean
+  allowClear?: boolean
+  onSearch?: (value: string) => void
 }
 export const SearchInput = (props: SearchInputProps) => {
   const {
@@ -70,36 +75,42 @@ export const SearchInput = (props: SearchInputProps) => {
   const inputRef = useRef(null)
 
   const clearValue = () => {
-    if(inputRef && inputRef.current) {
+    if (inputRef && inputRef.current) {
       inputRef.current.value = ''
     }
-    if(onSearch) {
+    if (onSearch) {
       onSearch('')
     }
   }
 
-  const inputProps: TextInputProps & {ref: any, isValid?: boolean} = {
+  const inputProps: TextInputProps & { ref: any; isValid?: boolean } = {
     ref: inputRef,
     isValid,
     autoFocus,
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
       const { value } = e.target
-      if(onSearch) {
+      if (onSearch) {
         onSearch(value)
       }
-      if(props.onChange) {
+      if (props.onChange) {
         props.onChange(e)
       }
     },
     ...restProps,
   }
 
-  return <Container width={'100%'} justify={'center'} paddingLeft={props.icon ? null : '24px'}>
-    <TextInputWrapper {...inputProps} />
-    {(allowClear && !props.disabled && props.value) &&
-      <InputSuffix>
-        <FormClose onClick={clearValue} />
-      </InputSuffix>
-    }
-  </Container>
+  return (
+    <Container
+      width={'100%'}
+      justify={'center'}
+      paddingLeft={props.icon ? null : '24px'}
+    >
+      <TextInputWrapper {...inputProps} />
+      {allowClear && !props.disabled && props.value && (
+        <InputSuffix>
+          <FormClose onClick={clearValue} />
+        </InputSuffix>
+      )}
+    </Container>
+  )
 }
