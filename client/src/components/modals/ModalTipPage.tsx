1528b5098745ca31974619668d757b1143c11380
fegloff <fegloff@gmail.com>
2023-03-28 18:55:44 -0500

    add transfer logic on ModalTipPage +  update modal calling on HomeDomainPage

--- client/src/components/modals/ModalTipPage.tsx
+++ client/src/components/modals/ModalTipPage.tsx
@@ -1,38 +1,93 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import { ModalContent } from './ModalContent'
 import { Title } from '../Text'
 import { CancelButton, Button } from '../Controls'
-import { Row } from '../Layout'
+import { FlexColumn, Row } from '../Layout'
+import { usePrepareSendTransaction, useSendTransaction } from 'wagmi'
+import Web3 from 'web3'
+import { DomainName } from '../Text'
+
+import { DomainLevel } from '../../api/utils'
+import { SearchInput } from '../search-input/SearchInput'
 
 interface Props {
   onClose?: () => void
+  domainName: string
+  ownerAddress: string
+  domainLevel: DomainLevel
 }
 
-export const ModalTipPage: React.FC<Props> = observer(({ onClose }) => {
-  return (
-    <ModalContent>
-      <Title>Tip this page</Title>
-      <div style={{ overflowY: 'auto', marginBottom: '1em' }}>Tip form</div>
+const defaultFormFields = {
+  to: '',
+  amount: '',
+}
 
-      <Row style={{ justifyContent: 'space-between' }}>
-        <CancelButton
-          type="submit"
-          onClick={() => {
-            onClose()
-          }}
-        >
-          Cancel
-        </CancelButton>
-        <Button
-          type="submit"
-          onClick={() => {
-            onClose()
-          }}
-        >
-          I agree
-        </Button>
-      </Row>
-    </ModalContent>
-  )
-})
+export const ModalTipPage: React.FC<Props> = observer(
+  ({ onClose, domainName, ownerAddress, domainLevel }) => {
+    const [amount, setAmount] = useState('')
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target
+      setAmount(value)
+    }
+    console.log(domainName, ownerAddress)
+    const { config } = usePrepareSendTransaction({
+      request: {
+        to: ownerAddress ? ownerAddress : undefined,
+        value: amount
+          ? Web3.utils.toBN(Web3.utils.toWei(amount)).toString()
+          : undefined,
+      },
+    })
+
+    const { sendTransaction } = useSendTransaction(config)
+
+    const formSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+      console.log('click')
+      sendTransaction && sendTransaction()
+    }
+
+    return (
+      <ModalContent>
+        <Title>P2P Transfer</Title>
+        <div style={{ overflowY: 'auto' }} />
+        <form onSubmit={formSubmit}>
+          <FlexColumn style={{ textAlign: 'center' }}>
+            <span style={{ fontSize: '1rem' }}>
+              You can tip the owner of this page
+            </span>
+            <DomainName
+              level={domainLevel}
+              style={{ fontSize: '1rem', marginBottom: '1.5em' }}
+            >
+              {domainName}
+            </DomainName>
+            <SearchInput
+              aria-label="Amount (ether)"
+              name="amount"
+              type="number"
+              required
+              onChange={onChange}
+              placeholder="Please enter the amount"
+            />
+            <Row
+              style={{ justifyContent: 'space-between', marginTop: '1.5em' }}
+            >
+              <CancelButton
+                type="submit"
+                onClick={() => {
+                  onClose()
+                }}
+              >
+                Cancel
+              </CancelButton>
+              <Button type="submit">Tip</Button>
+            </Row>
+          </FlexColumn>
+        </form>
+      </ModalContent>
+    )
+  }
+)
--- client/src/routes/home/components/HomeDomainPage.tsx
+++ client/src/routes/home/components/HomeDomainPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
+import { toast } from 'react-toastify'
 
 import { VanityURL } from '../VanityURL'
 import { WidgetModule } from '../../widgetModule/WidgetModule'
@@ -11,7 +12,7 @@ import { useStores } from '../../../stores'
 import { DomainName } from '../../../components/Text'
 import { Container, DomainNameContainer, TipPageButton } from '../Home.styles'
 import config from '../../../../config'
-import { getDomainLevel } from '../../../api/utils'
+import { DomainLevel, getDomainLevel } from '../../../api/utils'
 import { getDomainName } from '../../../utils/getDomainName'
 import { ModalIds, ModalRegister } from '../../../modules/modals'
 import { modalStore } from '../../../modules/modals/ModalContext'
@@ -21,6 +22,7 @@ interface Props {}
 
 const HomeDomainPage: React.FC<Props> = observer(() => {
   const [domainName] = useState(getDomainName())
+  const [level, setLevel] = useState<DomainLevel>('common')
   const { domainStore, walletStore, metaTagsStore } = useStores()
 
   // useEffect(() => {
@@ -29,6 +31,7 @@ const HomeDomainPage: React.FC<Props> = observer(() => {
   useEffect(() => {
     if (domainName) {
       domainStore.loadDomainRecord(domainName)
+      setLevel(getDomainLevel(domainStore.domainName))
     }
   }, [domainName])
 
@@ -44,7 +47,11 @@ const HomeDomainPage: React.FC<Props> = observer(() => {
 
   const openModal = (event: React.MouseEvent<HTMLSpanElement>) => {
     event.preventDefault()
-    modalStore.showModal(ModalIds.TIP_PAGE)
+    if (walletStore.isConnected) {
+      modalStore.showModal(ModalIds.TIP_PAGE)
+    } else {
+      toast.error('Plase connect your wallet', { autoClose: 2000 })
+    }
   }
 
   const showRenewalBlock =
@@ -59,15 +66,17 @@ const HomeDomainPage: React.FC<Props> = observer(() => {
       <div style={{ height: '2em' }} />
       <DomainNameContainer>
         <DomainName
-          level={getDomainLevel(domainStore.domainName)}
+          level={level}
           onClick={handleClickDomain}
           style={{ cursor: widgetListStore.txDomain && 'pointer' }}
         >
           {domainStore.domainName}.country
         </DomainName>
-        <TipPageButton>
-          <button onClick={openModal}>Tip me!</button>
-        </TipPageButton>
+        {domainStore.domainRecord && domainStore.domainRecord.renter && (
+          <TipPageButton>
+            <button onClick={openModal}>Tip me!</button>
+          </TipPageButton>
+        )}
       </DomainNameContainer>
 
       {domainStore.domainRecord && domainStore.domainRecord.renter && (
@@ -77,10 +86,17 @@ const HomeDomainPage: React.FC<Props> = observer(() => {
       <HomePageFooter />
       <div style={{ height: 200 }} />
       <ModalRegister
-        layerProps={{ position: 'center', full: 'vertical' }}
+        layerProps={{ position: 'center', full: false }}
         modalId={ModalIds.TIP_PAGE}
       >
-        {(modalProps) => <ModalTipPage {...modalProps} />}
+        {(modalProps) => (
+          <ModalTipPage
+            domainLevel={level}
+            domainName={`${domainName}${config.tld}`}
+            ownerAddress={domainStore.domainRecord.renter}
+            {...modalProps}
+          />
+        )}
       </ModalRegister>
     </Container>
   )
