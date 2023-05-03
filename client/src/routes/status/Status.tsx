891d44458fe81c4bb4cde44e72633b840b785069
fegloff <fegloff@gmail.com>
2023-05-02 23:13:05 -0500

    change iframe width

--- client/src/routes/status/Status.tsx
+++ client/src/routes/status/Status.tsx
@@ -5,24 +5,25 @@ import { observer } from 'mobx-react-lite'
 import { Title } from '../../components/Text'
 
 const Stats = observer(() => {
-
   return (
     <Container>
-        <FlexColumn style={{ width: '100%', height: '100%', alignItems: 'center' }}>
-          <Title>1Country Status</Title>
-          <div id='main_block'>
-            <iframe
-              title='Status'
-              src='https://1country.betteruptime.com'
-              frameBorder='0'
-              width='1000'
-              height='650'
-              // allowtransparency
-            />
+      <FlexColumn
+        style={{ width: '100%', height: '100%', alignItems: 'center' }}
+      >
+        <Title>1Country Status</Title>
+        <div id="main_block">
+          <iframe
+            title="Status"
+            src="https://1country.betteruptime.com"
+            frameBorder="0"
+            width="800"
+            height="650"
+            // allowtransparency
+          />
           <br />
           <br />
-          </div>
-        </FlexColumn>
+        </div>
+      </FlexColumn>
     </Container>
   )
 })
