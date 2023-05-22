05f5e3ea7a8c98288f47d6fdc2b3acf87d333f54
artemkolodko <temakolodko@gmail.com>
2023-05-22 19:29:41 +0100

    Change default payments api url

--- client/config.ts
+++ client/config.ts
@@ -116,7 +116,7 @@ const config = {
         testnet: true,
       },
   payments: {
-    apiUrl: process.env.PAYMENTS_API_URL || 'http://localhost:3001',
+    apiUrl: process.env.PAYMENTS_API_URL || 'https://payments-api.fly.dev',
     stripePubKey: process.env.STRIPE_PUB_KEY || ''
   },
   embedly: {
