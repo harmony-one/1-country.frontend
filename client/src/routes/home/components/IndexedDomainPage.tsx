6868aa1d702050e483e279acffcadad96eefbd00
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-01-30 09:52:23 -0800

    indexed domain page redirect logic

--- client/src/routes/home/components/IndexedDomainPage.tsx
+++ client/src/routes/home/components/IndexedDomainPage.tsx
@@ -2,29 +2,38 @@ import React, { useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
 import axios from 'axios'
 
-import { VanityURL } from '../VanityURL'
 import { DomainRecordRenewal } from './DomainRecordRenewal'
 import { HomePageFooter } from './HomePageFooter'
 import { useStores } from '../../../stores'
 
-import { DomainName } from '../../../components/Text'
 import { Container } from '../Home.styles'
 import config from '../../../../config'
-import { getDomainLevel } from '../../../api/utils'
 import { getDomainName } from '../../../utils/urlHandler'
 
 import TweetEmbed from 'react-tweet-embed'
-import Web3 from 'web3'
+
+const currentPath = window.location.pathname.replace('/', '')
 
 interface Props {}
 const IndexedDomainPage: React.FC<Props> = observer(() => {
   const [domainName] = useState(getDomainName())
   const [tweetId, setTweetId] = useState('')
-  // const [bidPrice, setBidPrice] = useState('')
 
   const { domainStore, walletStore, metaTagsStore } = useStores()
 
   useEffect(() => {
+    const fetchRedirect = async (path: string) => {
+      try {
+        const response = await axios.get(
+          `https://inscription-indexer.fly.dev/domain/${domainName}/${path}`
+        )
+        console.log('### Redirecting to', response.data.url)
+        return response.data.url
+      } catch (error) {
+        console.error('Error fetching redirect link:', error)
+      }
+    }
+
     const fetchDomainData = async () => {
       try {
         const response = await axios.get(
@@ -34,7 +43,6 @@ const IndexedDomainPage: React.FC<Props> = observer(() => {
         if (data) {
           const url = data.url.replace('x.com', 'twitter.com')
           setTweetId(extractTweetId(url))
-          // setBidPrice(data.gasPrice)
         }
       } catch (error) {
         console.error('Error fetching data:', error)
@@ -47,10 +55,20 @@ const IndexedDomainPage: React.FC<Props> = observer(() => {
       return match ? match[1] : ''
     }
 
+    if (currentPath) {
+      console.log('### Path:', currentPath)
+      fetchRedirect(currentPath).then((redirectLink) => {
+        if (redirectLink) {
+          window.location.href = redirectLink
+        }
+      })
+      return
+    }
+
     if (domainName) {
       fetchDomainData()
     }
-  }, [domainName])
+  }, [domainName, currentPath])
 
   useEffect(() => {
     if (domainName) {
@@ -64,16 +82,13 @@ const IndexedDomainPage: React.FC<Props> = observer(() => {
   const showRenewalBlock =
     walletStore.isConnected && domainStore.isOwner && domainStore.isExpired
 
+  if (currentPath) {
+    return null // prevent rendering if there's a path that requires redirection
+  }
+
   return (
     <Container>
-      <VanityURL
-        record={domainStore.domainRecord}
-        name={domainStore.domainName}
-      />
       <div style={{ height: '2em' }} />
-      {/* {bidPrice && (
-        <small>Highest Bid: {Web3.utils.fromWei(bidPrice, 'gwei')} GWEI</small>
-      )} */}
       {tweetId && (
         <div style={{ width: '100%' }}>
           <TweetEmbed tweetId={tweetId} options={{ width: 550 }} />
